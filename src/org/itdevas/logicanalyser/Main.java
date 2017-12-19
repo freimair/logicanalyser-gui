d58c14842a360cb4f3647a5df3bf78644cb1711c
Florian Reimair <florian.reimair@itdevas.at>
2017-12-19 21:16:48 +0100

    display full 8 bits

--- src/org/itdevas/logicanalyser/Main.java
+++ src/org/itdevas/logicanalyser/Main.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,16 +20,22 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Hello World!");
 
-		HBox root = new HBox();
+		VBox root = new VBox();
+
+		HBox sequence[] = { new HBox(), new HBox(), new HBox(), new HBox(), new HBox(), new HBox(), new HBox(),
+				new HBox() };
 
 		List<Character> data = getData();
 
 		for (Character current : data) {
-			Label tmp = new Label();
-			tmp.setText(0 == (current.charValue() & 1) ? "0" : "1");
-
-			root.getChildren().add(tmp);
+			for (int i = 0; i < sequence.length; i++) {
+				Label tmp = new Label();
+				tmp.setText(0 == (current.charValue() & (1 << i)) ? "0" : "1");
+				sequence[i].getChildren().add(tmp);
+			}
 		}
+
+		root.getChildren().addAll(sequence);
 
 		primaryStage.setScene(new Scene(root, 1000, 500));
 		primaryStage.show();
