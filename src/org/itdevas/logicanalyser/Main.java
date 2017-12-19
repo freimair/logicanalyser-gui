b2d9f67ce097ae433503dbf252c724673189d9fc
Florian Reimair <florian.reimair@itdevas.at>
2017-12-19 22:05:09 +0100

    added vertical line for state change

--- src/org/itdevas/logicanalyser/Main.java
+++ src/org/itdevas/logicanalyser/Main.java
@@ -40,13 +40,16 @@ public class Main extends Application {
 		long start = System.currentTimeMillis();
 
 		for (int i = 0; i < sequence.length; i++) {
-			for (int current = 0; current < data.size(); current++) {
+			for (int current = 0; current < data.size() - 1; current++) {
 				GraphicsContext gc = sequence[i].getGraphicsContext2D();
 
 				if (0 == (data.get(current).charValue() & (1 << i)))
 					gc.strokeLine(current * width, height, current * width + width, height);
 				else
 					gc.strokeLine(current * width, 0, current * width + width, 0);
+
+				if ((data.get(current).charValue() & (1 << i)) != (data.get(current + 1).charValue() & (1 << i)))
+					gc.strokeLine((current + 1) * width, 0, (current + 1) * width, height);
 			}
 		}
 		root.getChildren().addAll(sequence);
@@ -63,7 +66,7 @@ public class Main extends Application {
 	private List<Character> getData() {
 		List<Character> result = new ArrayList<>();
 
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 101; i++)
 			for (int j = 0; j < new Random().nextInt(10); j++)
 				result.add(new Character((char) i));
 
