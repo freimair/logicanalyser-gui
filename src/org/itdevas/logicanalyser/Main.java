22be769b181befc2b5cf18700c70b6f3702bc3c7
Florian Reimair <florian.reimair@itdevas.at>
2017-12-19 21:43:24 +0100

    much better performance with canvas

--- src/org/itdevas/logicanalyser/Main.java
+++ src/org/itdevas/logicanalyser/Main.java
@@ -6,8 +6,8 @@ import java.util.Random;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,24 +20,31 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Hello World!");
 
-		VBox root = new VBox();
+		VBox root = new VBox(1);
 
-		HBox sequence[] = { new HBox(), new HBox(), new HBox(), new HBox(), new HBox(), new HBox(), new HBox(),
-				new HBox() };
+		// HBox sequence[] = { new HBox(), new HBox(), new HBox(), new HBox(), new
+		// HBox(), new HBox(), new HBox(),
+		// new HBox() };
+
+		Canvas sequence[] = { new Canvas(1000, 10), new Canvas(1000, 10), new Canvas(1000, 10), new Canvas(1000, 10),
+				new Canvas(1000, 10), new Canvas(1000, 10), new Canvas(1000, 10), new Canvas(1000, 10) };
 
 		List<Character> data = getData();
 
 		long start = System.currentTimeMillis();
 
-		for (Character current : data) {
-			for (int i = 0; i < sequence.length; i++) {
-				Label tmp = new Label();
-				tmp.setText(0 == (current.charValue() & (1 << i)) ? "0" : "1");
-				sequence[i].getChildren().add(tmp);
+		for (int i = 0; i < sequence.length; i++) {
+			for (int current = 0; current < data.size(); current++) {
+				GraphicsContext gc = sequence[i].getGraphicsContext2D();
+
+				if (0 == (data.get(current).charValue() & (1 << i)))
+					gc.strokeLine(current * 10, 10, current * 10 + 10, 10);
+				else
+					gc.strokeLine(current * 10, 0, current * 10 + 10, 0);
 			}
 		}
-
 		root.getChildren().addAll(sequence);
+
 		System.out.println("done setting things up " + (System.currentTimeMillis() - start));
 
 		start = System.currentTimeMillis();
@@ -51,7 +58,7 @@ public class Main extends Application {
 		List<Character> result = new ArrayList<>();
 
 		for (int i = 0; i < 100; i++)
-			for (int j = 0; j < new Random().nextInt(100); j++)
+			for (int j = 0; j < new Random().nextInt(10); j++)
 				result.add(new Character((char) i));
 
 		return result;
